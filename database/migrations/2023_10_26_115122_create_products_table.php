c91185527c46c76d03224d1c37dbebac56dfb97f
didardev007 <didarmeredow007007@gmail.com>
2023-10-30 00:14:14 +0500

    1.0.10

--- database/migrations/2023_10_26_115122_create_products_table.php
+++ database/migrations/2023_10_26_115122_create_products_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedDouble('price');
-            $table->text('description');
+            $table->unsignedDouble('price')->default(0);
+            $table->text('description')->default(0);
             $table->unsignedTinyInteger('discount_percent')->default(0);
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
